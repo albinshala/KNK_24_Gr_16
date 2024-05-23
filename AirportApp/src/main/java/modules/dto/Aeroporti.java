1baf1199b85edb82aacd60c0fba148150de63721
AlbionAhmeti <albionahmeti2003@gmail.com>
2024-05-23 14:24:00 +0200

    models.dto

---
+++ AirportApp/src/main/java/modules/dto/Aeroporti.java
@@ -0,0 +1,34 @@
+package models.dto;
+
+public class Aeroporti {
+    private int id;
+    private  String emri;
+    private String qyteti;
+
+    public Aeroporti(){
+
+    }
+    public Aeroporti(int id,String emri,String qyteti){
+        this.id=id;
+        this.emri=emri;
+        this.qyteti=qyteti;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getEmri(){
+        return emri;
+    }
+    public String getQyteti(){
+        return qyteti;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setEmri(String emri){
+        this.emri=emri;
+    }
+    public void setQyteti(String qyteti){
+        this.qyteti=qyteti;
+}
+}
---
+++ AirportApp/src/main/java/modules/dto/Airoplani.java
@@ -0,0 +1,51 @@
+package models.dto;
+
+public class Airoplani {
+    private int id;
+    String kompania;
+    private int kapaciteti;
+    private String tipi;
+
+    public Airoplani(){
+
+    }
+
+    public Airoplani(int id,String kompania, int kapaciteti, String tipi) {
+        this.id = id;
+        this.kompania = kompania;
+        this.kapaciteti = kapaciteti;
+        this.tipi = tipi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKompania() {
+        return kompania;
+    }
+
+    public void setKompania(String kompania) {
+        this.kompania = kompania;
+    }
+
+    public int getKapaciteti() {
+        return kapaciteti;
+    }
+
+    public void setKapaciteti(int kapaciteti) {
+        this.kapaciteti = kapaciteti;
+    }
+
+    public String getTipi() {
+        return tipi;
+    }
+
+    public void setTipi(String tipi) {
+        this.tipi=tipi;
+}
+}
