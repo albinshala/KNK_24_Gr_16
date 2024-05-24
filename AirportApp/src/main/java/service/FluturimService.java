c577f52dcecb52e77cf5fa6baaf56f38a57026f2
ritaademi <rita.ademi19@gmail.com>
2024-05-24 11:11:59 +0200

    e

--- AirportApp/src/main/java/service/FluturimService.java
+++ AirportApp/src/main/java/service/FluturimService.java
@@ -1,20 +1,31 @@
 package service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import models.Aeroporti;
+import models.Airoplani;
+import models.Bileta;
+import models.Fluturimet;
+import repository.AeroportiRepository;
+import repository.AiroplaniRepository;
+import repository.BiletaRepository;
+import repository.FluturimetRepository;
+
 import java.sql.SQLException;
 
-public class DBConnection {
-    private static Connection connection;
+public class FluturimService {
 
+    public static int aeroplaniId;
+    public static int aeroportiNisjesiId;
+    public static int aeroportiArritjesiId;
 
-    public static Connection getConnection() throws SQLException{
-        if (connection == null || connection.isClosed()) {
-            String url = "jdbc:mysql://localhost:3306/menaxhimi_i_fluturimeve_airoport";
-            String user = "root";
-            String password = "ritaademi";
-            connection = DriverManager.getConnection(url, user, password);
-        }
-        return connection;
+    public static void shtoFluturim(Fluturimet fluturim) throws SQLException {
+
+        FluturimetRepository.insert(fluturim);
+
+    }
+
+    public static void shtoObjektetTjera(Airoplani airoplani, Aeroporti aeroporti_nisjes, Aeroporti aeroporti_arritjes) throws Exception{
+        aeroplaniId = AiroplaniRepository.insert(airoplani);
+        aeroportiNisjesiId = AeroportiRepository.insert(aeroporti_nisjes);
+        aeroportiArritjesiId = AeroportiRepository.insert(aeroporti_arritjes);
     }
 }
