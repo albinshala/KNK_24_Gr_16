8c20db4fbc1994c63c235c37cb33f46aeaf71e1a
ritaademi <rita.ademi19@gmail.com>
2024-05-24 10:08:11 +0200

    Update AeroportiRepository.java

--- AirportApp/src/main/java/repository/AeroportiRepository.java
+++ AirportApp/src/main/java/repository/AeroportiRepository.java
@@ -1,85 +1,64 @@
 package repository;
 
-
-import models.Airoplani;
+import models.Aeroporti;
 import service.DBConnection;
 
 import java.sql.*;
 
-public class AiroplaniRepository {
-    public static int insert(Airoplani airoplani) throws SQLException {
-        String sql = "INSERT INTO aeroplanet(kompania,kapaciteti, tipi) VALUES (?,?,?)";
+public class AeroportiRepository {
+
+    public static int insert(Aeroporti aeroporti) throws SQLException {
+        String sql = "INSERT INTO aeroporti(emri,qyteti) VALUES (?,?)";
         Connection connection = DBConnection.getConnection();
         PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        statement.setString(1, airoplani.getKompania());
-        statement.setInt(2, airoplani.getKapaciteti());
-        statement.setString(3, airoplani.getTipi());
+        statement.setString(1, aeroporti.getEmri());
+        statement.setString(2, aeroporti.getQyteti());
         statement.executeUpdate();
 
         ResultSet generatedKeys = statement.getGeneratedKeys();
-        int airoplaniId;
+        int aeroportiId;
         if (generatedKeys.next()) {
-            airoplaniId = generatedKeys.getInt(1);// përdor airoplaniId në shtimin e një rekordi në tabelën aeroplanet
+            aeroportiId = generatedKeys.getInt(1);// përdor aeroportiId në shtimin e një rekordi në tabelën aeroporti
         } else {
-            throw new SQLException("Can not get id of added plane!");
+            throw new SQLException("Can not get id of added airport!");
         }
 
-        return airoplaniId;
+        return aeroportiId;
     }
 
-    public static Airoplani getById(int id) throws SQLException {
-        String sql = "SELECT * FROM aeroplanet WHERE id=?";
+    public static Aeroporti getById(int id) throws SQLException {
+        String sql = "SELECT * FROM aeroporti WHERE id=?";
         try (Connection connection = DBConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                String kompania = resultSet.getString("kompania");
-                int kapaciteti = resultSet.getInt("kapaciteti");
-                String tipi = resultSet.getString("tipi");
-                return new Airoplani(id, kompania, kapaciteti, tipi);
+                String emri = resultSet.getString("emri");
+                String qyteti = resultSet.getString("qyteti");
+                return new Aeroporti(id, emri, qyteti);
             } else {
                 return null;
             }
         }
     }
 
-
-
-    public static void update(Airoplani airoplani) throws SQLException {
-        String sql = "UPDATE aeroplanet SET kompania=?, kapaciteti=?, tipi=?  WHERE id=?";
+    public static void update(Aeroporti aeroporti) throws SQLException {
+        String sql = "UPDATE aeroporti SET emri=?, qyteti=?  WHERE id=?";
         try (Connection connection = DBConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1,airoplani.getKompania());
-            statement.setInt(2, airoplani.getKapaciteti());
-            statement.setString(3, airoplani.getTipi());
+            statement.setString(1, aeroporti.getEmri());
+            statement.setString(2, aeroporti.getQyteti());
+            statement.setInt(3, aeroporti.getId());
             statement.executeUpdate();
         }
     }
 
     public static void delete(int id) throws SQLException {
-        String sql = "DELETE FROM aeroplanet WHERE id=?";
+        String sql = "DELETE FROM aeroporti WHERE id=?";
         try (Connection connection = DBConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             statement.executeUpdate();
         }
-    }
-
-    public static boolean intoCapacity(int seat, int fId) throws SQLException {
-        String sql = "SELECT a.kapaciteti FROM aeroplanet a INNER JOIN fluturimet f ON a.id = f.aeroplani_id WHERE f.id = ?";
-        Connection connection = DBConnection.getConnection();
-        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        statement.setInt(1, fId);
-
-        ResultSet resultSet = statement.executeQuery();
-        int kapaciteti = 0;
-        if (resultSet.next()){
-            kapaciteti = resultSet.getInt("a.kapaciteti");
-        }
-        if (seat > 0 && seat <= kapaciteti){
-            return true;
-        }
-        return false;
     }
 }
