8a4781d9df8165b6e0b08007ab0c7ada22250452
ritaademi <rita.ademi19@gmail.com>
2024-05-24 10:52:36 +0200

    e

--- AirportApp/src/main/java/service/FluturimService.java
+++ AirportApp/src/main/java/service/FluturimService.java
@@ -1,4 +1,20 @@
 package service;
 
-public class FluturimService {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static Connection connection;
+
+
+    public static Connection getConnection() throws SQLException{
+        if (connection == null || connection.isClosed()) {
+            String url = "jdbc:mysql://localhost:3306/menaxhimi_i_fluturimeve_airoport";
+            String user = "root";
+            String password = "ritaademi";
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        return connection;
+    }
 }
