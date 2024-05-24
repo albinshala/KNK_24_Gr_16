b05b25e0ea9db24e2286a1a739309761925123f6
Albin Shala <albinshala81@gmail.com>
2024-05-24 12:15:47 +0200

    update

--- AirportApp/src/main/java/module-info.java
+++ AirportApp/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.airportapp {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.airportapp to javafx.fxml;
     exports com.example.airportapp;
